f1d3367ba5d1b945a460e2fff957b4117a99915b
AlanCortez <acortez@vivipost.com>
2026-02-16 18:29:07 -0600

    backend iniciado

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/v1/auth', require('./routes/auth.routes'));
+app.use('/api/v1/users', require('./routes/user.routes'));
+app.use('/api/v1/products', require('./routes/product.routes'));
+app.use('/api/v1/sales', require('./routes/sale.routes'));
+app.use('/api/v1/dashboard', require('./routes/dashboard.routes'));
+
+module.exports = app;
